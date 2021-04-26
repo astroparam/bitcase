e8bf2b78a5cbbede903d027ad3b67ed86191eafe
Param Patidar <prm.mehul@gmail.com>
2021-04-26 08:45:33 +0530

    Discard negative value of id

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -36,7 +36,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil {
+	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
