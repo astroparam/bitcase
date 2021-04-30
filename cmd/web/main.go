0a7a17f6a7853e1145ae524da4d7df164f1ea0a7
Param Patidar <prm.mehul@gmail.com>
2021-04-30 21:05:28 +0530

    move routes to standalone file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,17 +13,10 @@ type application struct {
 }
 
 func main() {
-	// This essentially defines a new command-line flag with the name addr, a
-	// default value of ":4000" and some short help text explaining what the
-	// flag controls. The value of the flag will be stored in the addr variable at
-	// runtime.
-	addr := flag.String("addr", ":4000", "HTTP network address")
 
-	// flag.Parse() parse the command-line arguments update default
-	// values of the flag with provided value.
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	// log.New() create a logger for writing information messages.
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
@@ -31,26 +24,13 @@ func main() {
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
 
-	// Create a file server which serves files out of the "./ui/static" directory
-	// Note that the path given to the http.Dir function is relative to project's
-	// root directory
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Initialize a new http.Server struct.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
-	// The value returned from the flag.String() is pointer
-	// So addr variable is pointer to a string value. We need to
-	// dereference the pointer.
+
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
